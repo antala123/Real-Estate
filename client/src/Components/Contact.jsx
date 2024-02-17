eaac281b7c7d291c121bef8e7489c5bf31f57191
antala123 <aniketantalaa116@gmail.com>
2024-02-17 17:09:27 +0530

    real estate final project submit

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Contact({ listing }) {
+    console.log(listing);
 
     const [landlord, setLandlord] = useState(null);
     const [message, setMessage] = useState('');
