df0de84c62a491d4225679bd42631a9c0c91e444
antala123 <aniketantalaa116@gmail.com>
2024-02-19 11:44:27 +0530

    reset proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "https://real-estate-web-red.vercel.app/",
+        target: "http://localhost:3000",
         secure: false
       },
     }
